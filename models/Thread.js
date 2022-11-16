f6c655287eaae411648e5f91cef4b3f39156a7e0
Gabriel Rodriguez <garodri97@gmail.com>
2022-11-15 22:03:07 -0800

    added Schema for threads

---
+++ models/Thread.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const ThreadSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    likes: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    likedBy: {
+        type: [mongoose.Schema.Types.ObjectId]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
