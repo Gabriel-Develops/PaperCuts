ddcdfd3ae393ca906ac575b05d711c9f53712177
Gabriel Rodriguez <garodri97@gmail.com>
2022-11-16 22:25:07 -0800

    updated the way the front end receives data from the backend

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,11 @@
 const Bookclub = require('../models/Bookclub')
 
 exports.getIndex =  (req, res) => {
-    res.render('index.ejs', {loggedIn: false})
+    res.render('index.ejs', {
+        user: {
+            loggedIn: false
+        }
+    })
 }
 
 exports.getFeed = async (req, res) => {
@@ -9,11 +13,21 @@ exports.getFeed = async (req, res) => {
         // We are searching through the reader array in the bookclub objects
         // $elemMatch is not necessary because this is a single query condition
         const bookclubs = await Bookclub.find({readers: req.user.id})
-        res.render('feedReader', {loggedIn: true, bookclubs: bookclubs})
+        res.render('feedReader', {
+            user: {
+                loggedIn: false
+            }, 
+            bookclubs: bookclubs
+        })
     }
     else if (req.user.accountType === 'clubmaker') {
         const bookclubs = await Bookclub.find({clubmaker: req.user.id})
-        res.render('feedClubmaker', {loggedIn: true, bookclubs: bookclubs})
+        res.render('feedClubmaker', {
+            user: {
+                loggedIn: false
+            }, 
+            bookclubs: bookclubs
+        })
     }
     else {
         console.error('accountType incorrect', req.body.accountType)
