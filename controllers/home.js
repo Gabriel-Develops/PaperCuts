8f56e5669e491b8f2d48801fc07599e7f0a3b13c
Gabriel Rodriguez <garodri97@gmail.com>
2022-12-04 19:32:28 -0800

    Bookclubs are now displayed with more information on the feed page, styling still needs work

--- controllers/home.js
+++ controllers/home.js
@@ -1,4 +1,5 @@
 const Bookclub = require('../models/Bookclub')
+const Thread = require('../models/Thread')
 
 exports.getIndex =  (req, res) => {
     res.render('index.ejs', {
@@ -15,17 +16,37 @@ exports.getFeed = async (req, res) => {
             // We are searching through the reader array in the bookclub objects
             // $elemMatch is not necessary because this is a single query condition
             bookclubs = await Bookclub.find({readers: req.user.id})
-        }
-        else if (req.user.accountType === 'clubmaker') {
+        } else if (req.user.accountType === 'clubmaker') {
             bookclubs = await Bookclub.find({clubmaker: req.user.id})
         }
-    
+
+        const bookclubsForFE = []
+        for (let bookclub of bookclubs) {
+            // If there are threads in the bookclub, it will default to an empty object
+            let threadQuery = await Thread.findOne({bookclubId: bookclub._id}).sort({$natural:-1}).limit(1)
+            let recentThread
+            if (threadQuery) {
+                recentThread = {
+                    title: threadQuery.title,
+                    description: threadQuery.description
+                }
+            } else {
+                recentThread = null
+            }
+            bookclubsForFE.push({
+                id: bookclub._id,
+                name: bookclub.name,
+                clubId: bookclub.clubId,
+                readers: bookclub.readers,
+                recentThread: recentThread
+            })
+        }
         res.render('feed', {
             user: {
                 loggedIn: true,
                 accountType: req.user.accountType
             }, 
-            bookclubs: bookclubs
+            bookclubs: bookclubsForFE
         })
     } catch(e) {
         console.error(e)
